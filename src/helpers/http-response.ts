00323996230d8e828fbe10e3c739308c337d7983
blessingtutka <blessingtutka298@gmail.com>
2024-08-20 00:32:33 +0200

    refactor(http-response): update error responses

--- src/helpers/http-response.ts
+++ src/helpers/http-response.ts
@@ -5,17 +5,20 @@ class HttpResponse {
   data: any;
   message: string;
   status: 'success' | 'error';
+  error_code: string;
 
   constructor(
     status_code: number,
     status: 'success' | 'error',
     message: string = '',
     data: any = null,
+    error_code: string = '',
   ) {
     this.status_code = status_code;
     this.status = status;
     this.message = message;
     this.data = data;
+    this.error_code = error_code;
   }
 
   static success(
@@ -29,28 +32,41 @@ class HttpResponse {
   static serverError(
     message: string = 'An error occurred',
     status_code: number = 500,
-    data: any = null,
   ): HttpResponse {
-    return new HttpResponse(status_code, 'error', message, data);
+    return new HttpResponse(
+      status_code,
+      'error',
+      message,
+      null,
+      'Server Error',
+    );
   }
 
   static badRequest(
     message: string = 'Bad request',
     status_code: number = 400,
-    data: any = null,
   ): HttpResponse {
-    return new HttpResponse(status_code, 'error', message, data);
+    return new HttpResponse(status_code, 'error', message, null, 'Bad Request');
   }
 
   static notFound(
     message: string = 'Resource not found',
     status_code: number = 404,
-    data: any = null,
   ): HttpResponse {
-    return new HttpResponse(status_code, 'error', message, data);
+    return new HttpResponse(status_code, 'error', message, null, 'Not Found');
   }
 
   toJson() {
+    if (this.status === 'error') {
+      return {
+        status_code: this.status_code,
+        status: this.status,
+        error: {
+          code: this.error_code,
+          message: this.message,
+        },
+      };
+    }
     return {
       status_code: this.status_code,
       status: this.status,
@@ -58,7 +74,8 @@ class HttpResponse {
       data: this.data,
     };
   }
-  send(res: any) {
+
+  send(res: Response) {
     res.status(this.status_code).json(this.toJson());
   }
 }
