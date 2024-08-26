39272565674e28690e5c73220256de3e24a81572
blessingtutka <blessingtutka298@gmail.com>
2024-08-26 22:35:31 +0200

    refactor(auth): update auth routes

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
-import { register, login } from './auth.controller';
+import { register, login, profile } from './auth.controller';
+import { googleAuthCallback } from './oauth.controller';
+import authenticate, {
+  authenticateGoogle,
+  authenticateGoogleCallback,
+} from '../../middlewares/authenticate';
 
 const authRoutes = Router();
 
 authRoutes.post('/register', register);
 authRoutes.post('/login', login);
+authRoutes.get('/google', authenticateGoogle);
+authRoutes.get(
+  '/google/callback',
+  authenticateGoogleCallback,
+  googleAuthCallback,
+);
+authRoutes.get('/profile', authenticate, profile);
 
 export default authRoutes;
