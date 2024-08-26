886810e72dbaf47f5c8c63ee57c9d1815a5c9579
blessingtutka <blessingtutka298@gmail.com>
2024-08-26 22:28:01 +0200

    refactor(middleware): update authenticate middleware

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import passport from '../config/passport.config';
+import isExpiredToken from '../utils/isexpired-token';
+import extractTokenFromHeader from '../utils/extract-token';
 import config from '../config/server.config';
 import HttpResponse from '../helpers/http-response';
 
@@ -12,13 +15,7 @@ const authenticate = (
   res: Response,
   next: NextFunction,
 ) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    const response = HttpResponse.unAuthorized('No token provided');
-    return response.send(res);
-  }
-
-  const token = authHeader.split(' ')[1];
+  const token = extractTokenFromHeader(req);
 
   if (!token) {
     const response = HttpResponse.unAuthorized('No token provided');
@@ -26,13 +23,54 @@ const authenticate = (
   }
 
   try {
-    const decoded = jwt.verify(token, config.jwtSecret);
-    req.user = decoded;
+    const payload = jwt.verify(token, config.jwtSecret);
+    if (isExpiredToken(payload)) {
+      const response = HttpResponse.unAuthorized('token expires');
+      return response.send(res);
+    }
+    req.user = payload;
     next();
   } catch (error) {
-    const response = HttpResponse.unAuthorized('No token provided');
+    const response = HttpResponse.unAuthorized('Invalid token');
     return response.send(res);
   }
 };
 
+const authenticateGoogle = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  passport.authenticate('google', { scope: ['profile', 'email'] })(
+    req,
+    res,
+    next,
+  );
+};
+
+const authenticateGoogleCallback = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  passport.authenticate(
+    'google',
+    { failureRedirect: '/', scope: ['email', 'profile'] },
+    (err: any, user: any, info: any) => {
+      if (err) {
+        const response = HttpResponse.unAuthorized('Authentication error');
+        return response.send(res);
+      }
+      if (!user) {
+        const response = HttpResponse.unAuthorized('Unauthorized user');
+        return response.send(res);
+      }
+      req.user = user;
+      next();
+    },
+  )(req, res, next);
+};
+
 export default authenticate;
+
+export { authenticateGoogle, authenticateGoogleCallback };
