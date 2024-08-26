b3239dd58fcaaab94c3ab941cf2ddafc054d2dcd
blessingtutka <blessingtutka298@gmail.com>
2024-08-26 22:19:39 +0200

    feat(auth): add google auth strategy

---
+++ src/modules/auth/strategies/google.strategy.ts
@@ -0,0 +1,41 @@
+import { creategoogleAccount, getUserByGoogleId } from '../oauth.service';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { validateGoogleAuth } from '../../../validator/auth.validators';
+import { generateToken } from '../../../utils/generate-token';
+import config from '../../../config/server.config';
+
+const googleStartegy = new GoogleStrategy(
+  {
+    clientID: config.googleClientId,
+    clientSecret: config.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    const { id, name, emails, photos } = profile;
+
+    if (!emails || emails.length === 0) {
+      return done(new Error('No email found in user profile'), false);
+    }
+
+    const userInfo = {
+      username: name?.givenName || '',
+      email: emails[0].value,
+      googleId: id,
+      avatarUrl: photos ? photos[0].value : '',
+    };
+
+    try {
+      let user = await getUserByGoogleId(userInfo.googleId);
+
+      if (!user) {
+        user = await creategoogleAccount(userInfo);
+      }
+      const token = generateToken(user);
+      return done(null, { access_token: token, user: user });
+    } catch (error) {
+      return done(error, false);
+    }
+  },
+);
+
+export default googleStartegy;
