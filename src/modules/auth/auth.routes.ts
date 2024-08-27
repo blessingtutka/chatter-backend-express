cef6f5090d11fadf29793b28a31f760ccad0884c
blessingtutka <blessingtutka298@gmail.com>
2024-08-27 23:04:47 +0200

    feat(auth): add facebook auth routes

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { register, login, profile } from './auth.controller';
-import { googleAuthCallback } from './oauth.controller';
+import { googleAuthCallback, facebookAuthCallback } from './oauth.controller';
 import authenticate, {
   authenticateGoogle,
+  authenticateFacebook,
   authenticateGoogleCallback,
+  authenticateFacebookCallback,
 } from '../../middlewares/authenticate';
 
 const authRoutes = Router();
@@ -11,10 +13,16 @@ const authRoutes = Router();
 authRoutes.post('/register', register);
 authRoutes.post('/login', login);
 authRoutes.get('/google', authenticateGoogle);
+authRoutes.get('/facebook', authenticateFacebook);
 authRoutes.get(
   '/google/callback',
   authenticateGoogleCallback,
   googleAuthCallback,
+);
+authRoutes.get(
+  '/facebook/callback',
+  authenticateFacebookCallback,
+  facebookAuthCallback,
 );
 authRoutes.get('/profile', authenticate, profile);
 
