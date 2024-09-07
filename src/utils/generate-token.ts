66ee8b155b7ff86560c69fe02fc9e5d876c64a26
blessingtutka <blessingtutka298@gmail.com>
2024-09-07 23:46:11 +0200

    fix(util): jwt config export error

--- src/utils/generate-token.ts
+++ src/utils/generate-token.ts
@@ -1,11 +1,11 @@
 import jwt from 'jsonwebtoken';
-import config from '../config/server.config';
+import authConfig from '../config/auth.config';
 import { User } from '../config/db.config';
 
 export const generateToken = (user: User): string => {
   return jwt.sign(
     { userId: user.userId, email: user.email },
-    config.jwtSecret,
+    authConfig.jwt.secret,
     {
       expiresIn: '2d',
     },
