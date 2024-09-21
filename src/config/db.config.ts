da49adaf31c9d1232d457235dd67b821299f74b2
blessingtutka <blessingtutka298@gmail.com>
2024-09-21 22:45:02 +0200

    chore(config): update db config

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,8 +1,8 @@
 import { PrismaClient, UserType } from '@prisma/client';
-import type { User, Profile } from '@prisma/client';
+import type { User, Profile, Otp } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export { UserType, User, Profile };
+export { UserType, User, Profile, Otp };
 
 export default prisma;
