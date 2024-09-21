3188523632734f75b1b5b78df1639b48682be4d6
blessingtutka <blessingtutka298@gmail.com>
2024-09-21 22:46:16 +0200

    refactor(type): update types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -5,7 +5,31 @@ type EmailType = {
   text?: string;
   template?: string;
   context?: {
+    [key: string]: any;
+  };
+};
+
+type ResetEmailType = {
+  from: string;
+  to: string;
+  subject: string;
+  text?: string;
+  template?: string;
+  context?: {
     firstName?: string;
     resetLink: string;
+  };
+};
+
+type OtpEmailType = {
+  from: string;
+  to: string;
+  subject: string;
+  text?: string;
+  template?: string;
+  context?: {
+    firstName?: string;
+    otpCode: string;
+    expiresIn: number;
   };
 };
