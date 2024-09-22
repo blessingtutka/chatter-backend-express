8be044af3ce26c73ce0ef885e9fd9f65dfdc30f8
blessingtutka <blessingtutka298@gmail.com>
2024-09-22 22:35:31 +0200

    feat(email): add email verification controller

--- src/modules/email/email.controller.ts
+++ src/modules/email/email.controller.ts
@@ -27,3 +27,26 @@ export const handlePasswordResetRequest = async (
     return error_response.send(res);
   }
 };
+
+export const handleVerifyEmailRequest = async (req: Request, res: Response) => {
+  const { email, firstName } = req.body;
+  const verificationLink =
+    'https://chatter.com/verify-email?token=chatterToken';
+
+  const errors = await validateReset(req.body);
+  if (errors.length > 0) return res.status(422).json({ errors });
+
+  try {
+    await mailService.sendVerifyEmail(email, verificationLink, firstName || '');
+    const response = HttpResponse.success(
+      null,
+      'Password reset email sent successfully',
+    );
+    return response.send(res);
+  } catch (error) {
+    const error_response = HttpResponse.serverError(
+      'Failed to send password reset email',
+    );
+    return error_response.send(res);
+  }
+};
