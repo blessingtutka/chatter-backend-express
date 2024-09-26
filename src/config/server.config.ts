76330d83b14438d47c7fce4c70c445b166da06c9
blessingtutka <blessingtutka298@gmail.com>
2024-09-26 23:03:57 +0200

    chore(config): add otp schedule config

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -4,11 +4,13 @@ dotenv.config();
 interface Config {
   port: number;
   databaseUrl: string;
+  otpSchedule: string;
 }
 
 const config: Config = {
   port: parseInt(process.env.PORT || '3000', 10),
   databaseUrl: process.env.DATABASE_URL || '',
+  otpSchedule: '0 0 * * *',
 };
 
 export default config;
