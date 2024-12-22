d2452f655807264e303bd723a64b70c867090fcf
blessingtutka <blessingtutka298@gmail.com>
2024-12-22 23:32:27 +0100

    refactor(validator): update validators interfaces

--- src/validator/interfaces.ts
+++ src/validator/interfaces.ts
@@ -1,4 +1,20 @@
+import { UserType } from '../config/db.config';
 export interface ValidationError {
   field: string;
   message: string;
+}
+
+export interface UserInput {
+  user_id: string;
+  code: string;
+  email: string;
+  username: string;
+  type: UserType;
+  password: string;
+}
+export interface ClientInput {
+  email: string;
+  name: string;
+  message: string;
+  services: string[];
 }
