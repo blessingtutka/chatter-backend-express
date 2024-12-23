c35c807b0ab6b17fb055500920d42d96aa7c80e5
blessingtutka <blessingtutka298@gmail.com>
2024-12-23 10:30:36 +0100

    refactor(mail): add client mail service

--- src/modules/email/email.controller.ts
+++ src/modules/email/email.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import HttpResponse from '../../helpers/http-response';
 import * as mailService from '../email/email.service';
 import { validateReset } from '../../validator/auth.validators';
+import { validateClient } from '../../validator/client.validators';
 
 export const handlePasswordResetRequest = async (
   req: Request,
@@ -23,6 +24,26 @@ export const handlePasswordResetRequest = async (
   } catch (error) {
     const error_response = HttpResponse.serverError(
       'Failed to send password reset email',
+    );
+    return error_response.send(res);
+  }
+};
+
+export const handleClientMail = async (req: Request, res: Response) => {
+  const { email, name, message, services } = req.body;
+  const errors = await validateClient(req.body);
+  if (errors.length > 0) return res.status(422).json({ errors });
+
+  try {
+    await mailService.sendClientMail(email, message, name || '', services);
+    const response = HttpResponse.success(
+      null,
+      'Client email sent successfully',
+    );
+    return response.send(res);
+  } catch (error) {
+    const error_response = HttpResponse.serverError(
+      'Failed to send client email',
     );
     return error_response.send(res);
   }
